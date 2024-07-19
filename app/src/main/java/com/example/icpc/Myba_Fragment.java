ab79d70a4dac00d18a1612a5b3ac5d4c089df3ea
luke <manmail993@163.com>
2024-07-19 16:27:28 +0800

    贴吧界面UI设计第一次

--- app/src/main/java/com/example/icpc/Myba_Fragment.java
+++ app/src/main/java/com/example/icpc/Myba_Fragment.java
@@ -15,12 +15,6 @@ public class Myba_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        cardView.setRadius(8);//设置图片圆角的半径大小
-
-        cardView.setCardElevation(8);//设置阴影部分大小
-
-        cardView.setContentPadding(5, 5, 5, 5);//设置图片距离阴影大小
-
         return inflater.inflate(R.layout.fragment_myba, container, false);
     }
 }
