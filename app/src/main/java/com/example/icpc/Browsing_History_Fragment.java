bbfcbb7f9cf6bcd6303f71d7bb8c13b992329e37
luke <manmail993@163.com>
2024-07-19 23:42:56 +0800

    我的界面UI设计第二次

---
+++ app/src/main/java/com/example/icpc/Browsing_History_Fragment.java
@@ -0,0 +1,28 @@
+package com.example.icpc;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Browsing_History_Fragment extends Fragment {
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_browsing_history, container, false);
+    }
+}
